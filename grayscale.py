c23c0d991459e06d5cffb829f55d79b5fb17a7fd
foggynight <foggynight709@gmail.com>
2021-03-10 22:49:19 -0330

    Write grayscale video to file

--- grayscale.py
+++ grayscale.py
@@ -8,24 +8,39 @@
 # Released under the MIT license
 
 from sys import argv
-from time import sleep
 
 import cv2
 
 if __name__ == '__main__':
-    playback_fps = 60
-    source = cv2.VideoCapture(argv[1])
+    video_path = argv[1]
 
-    while True:
-        ret, img = source.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cv2.imshow(argv[1], gray)
+    source = cv2.VideoCapture(video_path)
+    if (source.isOpened() == False):
+        print(f"grayscale: error: failed to open '{video_path}'")
+        exit(1)
 
-        key = cv2.waitKey(1)
-        if key == ord('q'):
+    video_fps = float(source.get(cv2.CAP_PROP_FPS))
+    video_width = int(source.get(3))
+    video_height = int(source.get(4))
+
+    destination = cv2.VideoWriter(
+            'output.mp4',
+            cv2.VideoWriter_fourcc(*'mp4v'),
+            video_fps,
+            (video_width, video_height))
+
+    while source.isOpened():
+        ret, frame = source.read()
+
+        if not ret:
             break
 
-        sleep(1 / playback_fps)
+        # The frame must be in the BGR format to be written to file. The
+        # following converts to GRAY and back to BGR, making the frame
+        # grayscale while allowing it to be written to file.
+        destination.write(cv2.cvtColor(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR))
 
+    destination.release()
     source.release()
+
     cv2.destroyAllWindows()
